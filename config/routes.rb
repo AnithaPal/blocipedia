9280bdaeb0761a2afb111077804e63a33af8b9d7
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-10 10:06:39 -0400

    wiki feature is added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
 
-  get 'welcome/about'
+  
+  devise_for :users
+  resources :wikis
+  
+
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
