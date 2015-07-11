5c5f1bfaf80486c40d8a344f551cb905ee4e5e6f
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-11 13:54:03 -0400

    rescue_from method is customized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
-     redirect_to root_url, alert: exception.message
-  end  
+  # rescue_from Pundit::NotAuthorizedError do |exception|
+  #    redirect_to root_url, alert: exception.message
+  # end  
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+  def user_not_authorized
+   flash[:alert] ="You are not authorized to perform this action."
+   redirect_to(request.referrer || root_path)
+ end
+
+   
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
